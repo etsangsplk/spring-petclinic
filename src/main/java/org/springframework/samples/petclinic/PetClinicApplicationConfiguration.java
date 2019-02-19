0f41e8fdb27afa715ea5d27753de8d96012efb0d
Edward Tsang <etsang@splunk.com>
2019-02-18 16:15:51 -0800

    check in for now

---
+++ src/main/java/org/springframework/samples/petclinic/PetClinicApplicationConfiguration.java
@@ -0,0 +1,39 @@
+package org.springframework.samples.petclinic;
+
+import org.springframework.samples.tracing.TracerConfiguration;
+import org.springframework.samples.petclinic.PetClinicApplication;
+
+import io.opentracing.Tracer;
+import io.opentracing.util.GlobalTracer;
+import io.opentracing.contrib.spring.web.interceptor.TracingHandlerDecorator;
+import io.opentracing.contrib.spring.web.interceptor.HandlerInterceptorSpanDecorator;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class PetClinicApplicationConfiguration implements WebMvcConfigurer {
+    private static Logger LOGGER = LogManager.getLogger();
+
+    @Bean
+    public Tracer tracer() {
+        return (new TracerConfiguration()).getTracer(PetClinicApplication.SERVICE_NAME);
+    }
+
+    @Bean
+    public List<HandlerInterceptorSpanDecorator> spanDecorators() {
+        return Arrays.asList(new TracingHandlerDecorator());
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+
+        registry.addInterceptor(new TracingHandlerInterceptor(GlobalTracer.get(), spanDecorators()));
+        LOGGER.info("++++++++++++++++++++++++");
+    }
+
+}
---
+++ src/main/java/org/springframework/samples/tracing/TracingHandlerDecorator.java
@@ -0,0 +1,83 @@
+package org.springframework.samples.tracing;
+
+import io.opentracing.Span;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.web.method.HandlerMethod;
+import io.opentracing.contrib.spring.web.interceptor.HandlerInterceptorSpanDecorator;
+import org.bson.types.ObjectId;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Optional;
+
+public class TracingHandlerDecorator implements HandlerInterceptorSpanDecorator {
+    private static Logger LOGGER = LogManager.getLogger();
+
+    @Override
+    public void onPreHandle(HttpServletRequest httpServletRequest,
+                            Object handler,
+                            Span span) {
+        String metaData = HandlerUtils.methodName(handler);
+        LOGGER.info("*************tracinghandledecorator***********");
+        LOGGER.info("*******Global tracer*********** ={}", io.opentracing.util.GlobalTracer.get());
+        if (metaData != null) {
+            span.setOperationName(metaData);
+            //SplunkUri uri = new SplunkUri(httpServletRequest.getRequestURI());
+            //String tenantId = uri.getTenant();
+            String requestId = getRequestId(httpServletRequest);
+            span.setTag("requestId", requestId);
+            //span.setTag("tenant", tenantId);
+        }
+    }
+
+    @Override
+    public void onAfterCompletion(HttpServletRequest httpServletRequest,
+                                  HttpServletResponse httpServletResponse,
+                                  Object handler,
+                                  Exception ex,
+                                  Span span) {
+        LOGGER.info("*************tracinghandledecorator ONAFTERCOMPLETION ***********");
+    }
+
+    @Override
+    public void onAfterConcurrentHandlingStarted(HttpServletRequest httpServletRequest,
+                                                 HttpServletResponse httpServletResponse,
+                                                 Object handler,
+                                                 Span span) {
+        LOGGER.info("*************tracinghandledecorator AFTERCONCURRENTHANDLEING  STARTED***********");
+    }
+
+    private String getRequestId(HttpServletRequest request) {
+        return Optional
+            .ofNullable(request.getHeader("X-Request-Id"))
+            .orElseGet(() -> ObjectId.get().toString());
+    }
+
+    /**
+     * Helper class for deriving tags/logs from handler object.
+     */
+    static class HandlerUtils {
+        private HandlerUtils() {}
+
+        /**
+         * Class name of a handler serving request.
+         */
+        public static final String HANDLER_CLASS_NAME = "handler.class_simple_name";
+        /**
+         * Method name of handler serving request.
+         */
+        public static final String HANDLER_METHOD_NAME = "handler.method_name";
+        /**
+         * Spring handler object.
+         */
+        public static final String HANDLER = "handler";
+
+        public static String methodName(Object handler) {
+            return handler instanceof HandlerMethod
+                ? ((HandlerMethod) handler).getMethod().getName() : null;
+        }
+    }
+
+
+}
