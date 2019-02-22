1571cebe66b9d01983e9c7ceb2db7ee21cf2168c
Edward Tsang <etsang@splunk.com>
2019-02-22 13:29:00 -0800

    check in for now

--- src/test/java/org/springframework/samples/tracing/LightstepTest.java
+++ src/test/java/org/springframework/samples/tracing/LightstepTest.java
@@ -1,4 +1,19 @@
 package org.springframework.samples.tracing;
 
+import com.lightstep.tracer.jre.JRETracer;
+import io.opentracing.Tracer;
+import io.opentracing.noop.NoopTracerFactory;
+
+import org.junit.Assert;
+import org.junit.Test;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
 public class LightstepTest {
+    @Test
+    public void tracerInitializedWithServiceName() throws Exception {
+        Tracer tracer = Lightstep.registerTracer("test");
+        assertThat(tracer, instanceOf(JRETracer.class));
+
+    }
 }
